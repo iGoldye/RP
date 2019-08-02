7e500bbed13c8c264686fa271aa90189fa69e2f6
Dan Sadler <sad2004@mail.ru>
2019-08-02 22:03:59 +0600

    Sosamba UI: Custom notifications

--- server-data/resources/sosamba_ui/client/main.lua
+++ server-data/resources/sosamba_ui/client/main.lua
@@ -73,6 +73,51 @@ Citizen.CreateThread(function()
 	end
 end)
 
+function convertNotificationMessage(msg)
+	local function count(base, pattern)
+	    return select(2, string.gsub(base, pattern, ""))
+	end
+
+	local function convert_color_to_html(str, from ,to, endtag)
+		local result = str
+		result = result:gsub(from, to)
+		for i=1,count(str,from) do
+			result = result .. endtag
+		end
+		return result
+	end
+
+	local str = msg;
+	str = str:gsub("&", "&amp;")
+	str = str:gsub("<", "&lt;")
+	str = str:gsub(">", "&gt;")
+	str = str:gsub("\"", "&quot;")
+	str = str:gsub("~n~","<br>")
+	str = convert_color_to_html(str, "~r~", "<span class='notify-red'>", "</span>")
+	str = convert_color_to_html(str, "~b~", "<span class='notify-blue'>", "</span>")
+	str = convert_color_to_html(str, "~g~", "<span class='notify-green'>", "</span>")
+	str = convert_color_to_html(str, "~y~", "<span class='notify-yellow'>", "</span>")
+	str = convert_color_to_html(str, "~p~", "<span class='notify-purple'>", "</span>")
+	str = convert_color_to_html(str, "~o~", "<span class='notify-orange'>", "</span>")
+	str = convert_color_to_html(str, "~c~", "<span class='notify-gray'>", "</span>")
+	str = convert_color_to_html(str, "~m~", "<span class='notify-darkgray'>", "</span>")
+	str = convert_color_to_html(str, "~u~", "<span class='notify-black'>", "</span>")
+	str = convert_color_to_html(str, "~w~", "<span class='notify-white'>", "</span>")
+	str = convert_color_to_html(str, "~s~", "<span class='notify-default'>", "</span>")
+	str = convert_color_to_html(str, "~h~", "<span class='notify-bold'>", "</span>")
+
+	return str
+end
+
+RegisterNetEvent('sosamba_ui:showNotification')
+AddEventHandler('sosamba_ui:showNotification', function(args)
+
+	SendNUIMessage({
+		action  = 'showNotification',
+		text = convertNotificationMessage(args.text),
+	})
+end)
+
 RegisterNetEvent('sosamba_ui:toggleweb')
 AddEventHandler('sosamba_ui:toggleweb', function()
 	webShow = not webShow
