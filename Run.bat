73868b8870302e7f0079a7bd7fdf8b79f5213fb1
Skeev_ <derkach94@gmail.com>
2019-08-04 15:18:01 +0300

    update .gitignore

--- Run.bat
+++ Run.bat
@@ -1,2 +1,2 @@
-cd /d C:\FXServer\server-data
-C:\FXServer\server\run.cmd +exec server.cfg +set sv_licenseKey l53nt6s0s6derulxfb5ipddjmj7e7882
+cd /d E:\RP\server-data
+E:\RP\server\run.cmd +exec server.cfg
