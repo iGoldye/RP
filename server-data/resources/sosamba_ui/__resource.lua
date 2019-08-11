0b82efb300a7562cca177e1b28496cf53152ffb8
Dan Sadler <sad2004@mail.ru>
2019-08-11 22:18:56 +0600

    sosamba_ui: quick fix

--- server-data/resources/sosamba_ui/__resource.lua
+++ server-data/resources/sosamba_ui/__resource.lua
@@ -3,6 +3,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'Sosamba UI'
 version '0.1.0'
 
+--[[
 files {
   'html/index.html',
   'html/vue.js',
@@ -12,13 +13,30 @@ files {
   'html/fonts/opensans-regular.ttf',
   'html/fonts/opensans-semibold.ttf',
 }
+]]--
+
+files {
+	'html/js/app.js',
+	'html/css/app.css',
+	'html/css/chunk-vendors.css',
+	'html/chunk-vendors.bundle.js',
+	'html/favicon.ico',
+	'html/index.html',
+
+	'html/app.js',
+	'html/vendors~app.bundle.js',
+}
 
 server_scripts {
+  '@es_extended/locale.lua',
+  'locales/en.lua',
   'config.lua',
   'server/main.lua',
 }
 
 client_scripts {
+  '@es_extended/locale.lua',
+  'locales/en.lua',
   'config.lua',
   'client/main.lua',
 }
