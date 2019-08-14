ea439ef55a5d991b3afe61f09c2b74801e44c70a
Dan Sadler <sad2004@mail.ru>
2019-08-14 20:49:46 +0600

    admin_commands: /repair -- repair nearest car

--- server-data/resources/[development]/admin_commands/server/main.lua
+++ server-data/resources/[development]/admin_commands/server/main.lua
@@ -4,3 +4,10 @@ TriggerEvent('es:addGroupCommand', 'emote', 'admin', function(source, args, user
 end, function(source, args, user)
 	TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
 end, {help = "Get current player coordinates"})
+
+
+TriggerEvent('es:addGroupCommand', 'repair', 'admin', function(source, args, user)
+	TriggerClientEvent('admin_commands:repair', source)
+end, function(source, args, user)
+	TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
+end, {help = "Починить автомобиль"})
