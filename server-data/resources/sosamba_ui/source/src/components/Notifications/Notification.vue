94d726f130a0daa8eaa7120495ff2ea064afca26
Dan Sadler <sad2004@mail.ru>
2019-08-16 19:20:21 +0600

    gcphone: russian locale

--- server-data/resources/sosamba_ui/source/src/components/Notifications/Notification.vue
+++ server-data/resources/sosamba_ui/source/src/components/Notifications/Notification.vue
@@ -13,3 +13,42 @@ export default Vue.extend({
 })
 </script>
 
+<style>
+    .notify-red {
+        color: #F92652;
+    }
+    .notify-blue {
+        color: #66D9EF;
+    }
+    .notify-green {
+        color: #A6E22E;
+    }
+    .notify-yellow {
+        color: #E6DB74;
+    }
+    .notify-purple {
+        color: #AE81FF;
+    }
+    .notify-orange {
+        color: #FD971F;
+    }
+    .notify-gray {
+        color: #BCA3A3;
+    }
+    .notify-darkgray {
+        color: #8F8F8F;
+    }
+    .notify-black {
+        color: black;
+    }
+    .notify-white {
+        color: white;
+    }
+    .notify-default {
+        color: #F8F8F2;
+        font-weight: normal;
+    }
+    .notify-bold {
+        font-weight: bold;
+    }
+</style>
