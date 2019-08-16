224f7fa82be3653b76b4d4647af9eaa1588ad647
Nifly174 <53381847+Nifly174@users.noreply.github.com>
2019-08-16 18:17:27 +0500

    new car damage system

--- server-data/resources/RealisticVehicleFailure/config.lua
+++ server-data/resources/RealisticVehicleFailure/config.lua
@@ -15,21 +15,21 @@
 -- IMPORTANT: Some of these values MUST be defined as a floating point number. ie. 10.0 instead of 10
 
 cfg = {
-	deformationMultiplier = -1,					-- How much should the vehicle visually deform from a collision. Range 0.0 to 10.0 Where 0.0 is no deformation and 10.0 is 10x deformation. -1 = Don't touch. Visual damage does not sync well to other players.
+	deformationMultiplier = 1.5,					-- How much should the vehicle visually deform from a collision. Range 0.0 to 10.0 Where 0.0 is no deformation and 10.0 is 10x deformation. -1 = Don't touch. Visual damage does not sync well to other players.
 	deformationExponent = 0.4,					-- How much should the handling file deformation setting be compressed toward 1.0. (Make cars more similar). A value of 1=no change. Lower values will compress more, values above 1 it will expand. Dont set to zero or negative.
 	collisionDamageExponent = 0.6,				-- How much should the handling file deformation setting be compressed toward 1.0. (Make cars more similar). A value of 1=no change. Lower values will compress more, values above 1 it will expand. Dont set to zero or negative.
 
-	damageFactorEngine = 10.0,					-- Sane values are 1 to 100. Higher values means more damage to vehicle. A good starting point is 10
+	damageFactorEngine = 3.0,					-- Sane values are 1 to 100. Higher values means more damage to vehicle. A good starting point is 10
 	damageFactorBody = 10.0,					-- Sane values are 1 to 100. Higher values means more damage to vehicle. A good starting point is 10
 	damageFactorPetrolTank = 64.0,				-- Sane values are 1 to 200. Higher values means more damage to vehicle. A good starting point is 64
 	engineDamageExponent = 0.6,					-- How much should the handling file engine damage setting be compressed toward 1.0. (Make cars more similar). A value of 1=no change. Lower values will compress more, values above 1 it will expand. Dont set to zero or negative.
-	weaponsDamageMultiplier = 0.01,				-- How much damage should the vehicle get from weapons fire. Range 0.0 to 10.0, where 0.0 is no damage and 10.0 is 10x damage. -1 = don't touch
-	degradingHealthSpeedFactor = 10,			-- Speed of slowly degrading health, but not failure. Value of 10 means that it will take about 0.25 second per health point, so degradation from 800 to 305 will take about 2 minutes of clean driving. Higher values means faster degradation
-	cascadingFailureSpeedFactor = 8.0,			-- Sane values are 1 to 100. When vehicle health drops below a certain point, cascading failure sets in, and the health drops rapidly until the vehicle dies. Higher values means faster failure. A good starting point is 8
+	weaponsDamageMultiplier = 0.5,				-- How much damage should the vehicle get from weapons fire. Range 0.0 to 10.0, where 0.0 is no damage and 10.0 is 10x damage. -1 = don't touch
+	degradingHealthSpeedFactor = 5,			-- Speed of slowly degrading health, but not failure. Value of 10 means that it will take about 0.25 second per health point, so degradation from 800 to 305 will take about 2 minutes of clean driving. Higher values means faster degradation
+	cascadingFailureSpeedFactor = 3.0,			-- Sane values are 1 to 100. When vehicle health drops below a certain point, cascading failure sets in, and the health drops rapidly until the vehicle dies. Higher values means faster failure. A good starting point is 8
 
-	degradingFailureThreshold = 800.0,			-- Below this value, slow health degradation will set in
-	cascadingFailureThreshold = 360.0,			-- Below this value, health cascading failure will set in
-	engineSafeGuard = 100.0,					-- Final failure value. Set it too high, and the vehicle won't smoke when disabled. Set too low, and the car will catch fire from a single bullet to the engine. At health 100 a typical car can take 3-4 bullets to the engine before catching fire.
+	degradingFailureThreshold = 400.0,			-- Below this value, slow health degradation will set in
+	cascadingFailureThreshold = 200.0,			-- Below this value, health cascading failure will set in
+	engineSafeGuard = 50.0,					-- Final failure value. Set it too high, and the vehicle won't smoke when disabled. Set too low, and the car will catch fire from a single bullet to the engine. At health 100 a typical car can take 3-4 bullets to the engine before catching fire.
 
 	torqueMultiplierEnabled = true,				-- Decrease engine torque as engine gets more and more damaged
 
@@ -38,11 +38,11 @@ cfg = {
 
 	preventVehicleFlip = true,					-- If true, you can't turn over an upside down vehicle
 
-	sundayDriver = true,						-- If true, the accelerator response is scaled to enable easy slow driving. Will not prevent full throttle. Does not work with binary accelerators like a keyboard. Set to false to disable. The included stop-without-reversing and brake-light-hold feature does also work for keyboards.
+	sundayDriver = false,						-- If true, the accelerator response is scaled to enable easy slow driving. Will not prevent full throttle. Does not work with binary accelerators like a keyboard. Set to false to disable. The included stop-without-reversing and brake-light-hold feature does also work for keyboards.
 	sundayDriverAcceleratorCurve = 7.5,			-- The response curve to apply to the accelerator. Range 0.0 to 10.0. Higher values enables easier slow driving, meaning more pressure on the throttle is required to accelerate forward. Does nothing for keyboard drivers
 	sundayDriverBrakeCurve = 5.0,				-- The response curve to apply to the Brake. Range 0.0 to 10.0. Higher values enables easier braking, meaning more pressure on the throttle is required to brake hard. Does nothing for keyboard drivers
 
-	displayBlips = true,						-- Show blips for mechanics locations
+	displayBlips = false,						-- Show blips for mechanics locations
 
 	compatibilityMode = false,					-- prevents other scripts from modifying the fuel tank health to avoid random engine failure with BVA 2.01 (Downside is it disabled explosion prevention)
 
@@ -182,23 +182,23 @@ repairCfg = {
 	},
 
 	fixMessages = {
-		"You put the oil plug back in",
-		"You stopped the oil leak using chewing gum",
-		"You repaired the oil tube with gaffer tape",
-		"You tightened the oil pan screw and stopped the dripping",
-		"You kicked the engine and it magically came back to life",
-		"You removed some rust from the spark tube",
-		"You yelled at your vehicle, and it somehow had an effect"
+		"Вы воткнули пробку от масла обратно",
+		"Вы остановили утечку масла - жевательной резинкой",
+		"Вы отремонтировали масляную трубку с помощью изоленты",
+		"Вы затянули винт масляного поддона и остановили течь",
+		"Вы пнули двигатель, и он волшебным образом ожил",
+		"Вы удалили ржавчину с свечи зажигания",
+		"Вы закричали на свой автомобиль, и это как-то дало эффект"
 	},
 	fixMessageCount = 7,
 
 	noFixMessages = {
-		"You checked the oil plug. It's still there",
-		"You looked at your engine, it seemed fine",
-		"You made sure that the gaffer tape was still holding the engine together",
-		"You turned up the radio volume. It just drowned out the weird engine noises",
-		"You added rust-preventer to the spark tube. It made no difference",
-		"Never fix something that ain't broken they said. You didn't listen. At least it didn't get worse"
+		"Вы проверили мяслянную заглушку. Она на всё ещё на месте",
+		"Вы посмотрели на двигатель - он выглядит нормально",
+		"Вы убедились в том, что изолента всё ещё держит двигатель в одном целом",
+		"Вы прибавили громкости на радио. Это просто приглушило странные звуки из двигателя",
+		"Вы добавили средство против ржавчины в свечу зажигания. Ничего не изменилось",
+		"Никогда не ремонтируй то, что не сломано - говорили они. Вы не послушали. В любом случае, хуже не стало"
 	},
 	noFixMessageCount = 6
 }
