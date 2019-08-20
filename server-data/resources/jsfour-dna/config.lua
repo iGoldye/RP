bc0997f0477e046f095615e9ccea38f9c1c73bc9
Skeev_ <derkach94@gmail.com>
2019-08-20 22:23:44 +0300

    locale fix

--- server-data/resources/jsfour-dna/config.lua
+++ server-data/resources/jsfour-dna/config.lua
@@ -3,6 +3,6 @@ Config.Computer = {
 		Pos   = {x = 459.7448425293, y = -988.79364013672, z = 23.984875030518},
 		Size  = {x = 2.0, y = 2.0, z = 1.0},
 		Color = {r = 26, g = 55, b = 186},
-		Hint = 'Press ~INPUT_PICKUP~ to use the DNA-database',
+		Hint = 'Нажмите ~INPUT_PICKUP~ для подключения к базе ДНК',
 		Type = 27,
 }
