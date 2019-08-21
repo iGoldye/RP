d8b2af11db1fecb580874459675cc23007171450
mderkach <derkach94@gmail.com>
2019-08-21 14:32:18 +0300

    fix

--- server-data/resources/[esx]/esx_holdupbank/locales/en.lua
+++ server-data/resources/[esx]/esx_holdupbank/locales/en.lua
@@ -6,7 +6,7 @@ Locales['en'] = {
 	['press_to_rob'] = 'Нажмите ~INPUT_CONTEXT~ что бы начать взлом ~b~',
 	['robbery_of'] = 'Процесс взлома: ~r~',
 	['seconds_remaining'] = '~w~ секунд осталось',
-	['robbery_cancelled_at'] = '~r~ Взлома отменится через: ~b~',
+	['robbery_cancelled_at'] = '~r~ Взлом отменится через: ~b~',
 	['robbery_has_cancelled'] = '~r~ Взлом прекратиться: ~b~',
 	['already_robbed'] = 'Этот магазин уже был ограблен, Ждите: ',
 	['seconds'] = 'Секунд.',
@@ -16,7 +16,7 @@ Locales['en'] = {
 	['alarm_triggered'] = 'Сработала сигнализация',
 	['hold_pos'] = 'Ожидайте!',
 	['robbery_complete'] = '~r~ Успешный взлом!.~s~ ~h~  ',
-	['robbery_complete_at'] = '~r~ Взлома закончится через: ~b~',
+	['robbery_complete_at'] = '~r~ Взлом закончится через: ~b~',
 	['min_two_police'] = 'Нужно больше копов на смене, хотя бы: ',
 	['robbery_already'] = '~r~Взлом уже в процессе.',
 
