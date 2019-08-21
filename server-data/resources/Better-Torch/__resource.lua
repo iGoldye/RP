8029f8ed27801321cdce6e0dc83a348fc641e0fc
mderkach <derkach94@gmail.com>
2019-08-21 20:26:43 +0300

    shield + torch + dashcam +fixs

---
+++ server-data/resources/Better-Torch/__resource.lua
@@ -0,0 +1,8 @@
+--[[
+	Scripted By: Xander1998 (X. Cross)
+--]]
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+server_script "BetterTorch.Server.net.dll"
+client_script "BetterTorch.Client.net.dll"
