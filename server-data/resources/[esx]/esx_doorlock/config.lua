54423bc6f4ed48977dd6233115f0cbba4204041e
mderkach <derkach94@gmail.com>
2019-08-29 09:51:48 +0300

    doorlock, vehiclemanager fix

--- server-data/resources/[esx]/esx_doorlock/config.lua
+++ server-data/resources/[esx]/esx_doorlock/config.lua
@@ -312,16 +312,16 @@ Config.DoorList = {
 	},
 
 	-- Back Gate
-	{
-		objName = 'hei_prop_station_gate',
-		objYaw = 90.0,
-		objCoords  = vector3(488.8, -1017.2, 27.1),
-		textCoords = vector3(488.8, -1020.2, 30.0),
-		authorizedJobs = { 'police' },
-		locked = true,
-		distance = 14,
-		size = 2
-	},
+	-- {
+	-- 	objName = 'hei_prop_station_gate',
+	-- 	objYaw = 90.0,
+	-- 	objCoords  = vector3(488.8, -1017.2, 27.1),
+	-- 	textCoords = vector3(488.8, -1020.2, 30.0),
+	-- 	authorizedJobs = { 'police' },
+	-- 	locked = true,
+	-- 	distance = 14,
+	-- 	size = 2
+	-- },
 
 	--
 	-- Sandy Shores
