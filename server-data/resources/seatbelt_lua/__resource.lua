4a9413b101885c68704c160ce609f9eabe3c18b3
Dan Sadler <sad2004@mail.ru>
2019-08-29 16:12:31 +0600

    New resource: seatbelt_lua

---
+++ server-data/resources/seatbelt_lua/__resource.lua
@@ -0,0 +1,20 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/ru.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+	'html/app.js',
+	'html/style.css',
+	'html/img/seatbelt.png',
+	'html/sounds/buckle.ogg',
+	'html/sounds/unbuckle.ogg',
+	'html/sounds/chime.ogg'
+}
