879f2dde73fc195f0073c06144acd5eb90a70e01
Dan Sadler <sad2004@mail.ru>
2019-09-13 00:11:10 +0600

    clothesoff: female shoes off fix

--- server-data/resources/clothesoff/client.lua
+++ server-data/resources/clothesoff/client.lua
@@ -7,11 +7,9 @@ Citizen.CreateThread(function()
   end
 end)
 
-RegisterNetEvent('smerfikubrania:koszulka')
-AddEventHandler('smerfikubrania:koszulka', function()
+RegisterNetEvent('clothesoff:overdress')
+AddEventHandler('clothesoff:overdress', function()
 	TriggerEvent('skinchanger:getSkin', function(skin)
-	
-
 		local clothesSkin = {
 		['tshirt_1'] = 15, ['tshirt_2'] = 0,
 		['torso_1'] = 15, ['torso_2'] = 0,
@@ -20,38 +18,43 @@ AddEventHandler('smerfikubrania:koszulka', function()
 		TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
 	end)
 end)
-RegisterNetEvent('smerfikubrania:spodnie')
-AddEventHandler('smerfikubrania:spodnie', function()
+RegisterNetEvent('clothesoff:pants')
+AddEventHandler('clothesoff:pants', function()
 	TriggerEvent('skinchanger:getSkin', function(skin)
-	
+		local clothesSkin = nil
 
-		local clothesSkin = {
-		['pants_1'] = 21, ['pants_2'] = 0
-		}
+		if skin.sex == 1 then
+			clothesSkin = {['pants_1'] = 15, ['pants_2'] = 0}
+		else
+			clothesSkin = {['pants_1'] = 21, ['pants_2'] = 0}
+		end
+
 		TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
 	end)
 end)
 
-RegisterNetEvent('smerfikubrania:buty')
-AddEventHandler('smerfikubrania:buty', function()
+RegisterNetEvent('clothesoff:shoes')
+AddEventHandler('clothesoff:shoes', function()
 	TriggerEvent('skinchanger:getSkin', function(skin)
-	
+		local clothesSkin
 
-		local clothesSkin = {
-		['shoes_1'] = 34, ['shoes_2'] = 0
-		}
+		if skin.sex == 1 then
+			clothesSkin = {['shoes_1'] = 35, ['shoes_2'] = 0}
+		else
+			clothesSkin = {['shoes_1'] = 34, ['shoes_2'] = 0}
+		end
+
 		TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
 	end)
 end)
 
 function OpenActionMenuInteraction(target)
-
 	local elements = {}
 
-	table.insert(elements, {label = ('Одеться'), value = 'ubie'})
-	table.insert(elements, {label = ('Снять верх'), value = 'tul'})
-	table.insert(elements, {label = ('Снять низ'), value = 'spo'})
-	table.insert(elements, {label = ('Снять обувь'), value = 'but'})
+	table.insert(elements, {label = ('Одеться'), value = 'clotheson'})
+	table.insert(elements, {label = ('Снять верх'), value = 'overdress'})
+	table.insert(elements, {label = ('Снять низ'), value = 'pants'})
+	table.insert(elements, {label = ('Снять обувь'), value = 'shoes'})
 --  		ESX.UI.Menu.CloseAll()
 	ESX.UI.Menu.Open(
 		'default', GetCurrentResourceName(), 'action_menu',
@@ -61,16 +64,16 @@ function OpenActionMenuInteraction(target)
 			elements = elements
 		},
 	    function(data, menu)
-		if data.current.value == 'ubie' then			
+		if data.current.value == 'clotheson' then
 			ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin)
 				TriggerEvent('skinchanger:loadSkin', skin)
 			end)
-		elseif data.current.value == 'tul' then
-			TriggerEvent('smerfikubrania:koszulka')
-		elseif data.current.value == 'spo' then
-			TriggerEvent('smerfikubrania:spodnie')
-		elseif data.current.value == 'but' then
-			TriggerEvent('smerfikubrania:buty')
+		elseif data.current.value == 'overdress' then
+			TriggerEvent('clothesoff:overdress')
+		elseif data.current.value == 'pants' then
+			TriggerEvent('clothesoff:pants')
+		elseif data.current.value == 'shoes' then
+			TriggerEvent('clothesoff:shoes')
 	        end
 		menu.close()
 	    end,
@@ -78,20 +81,7 @@ function OpenActionMenuInteraction(target)
 		menu.close()
 	    end
 	)
-
-
 end
-
---[[
-Citizen.CreateThread(function()
-  while true do
-    Citizen.Wait(0)
-    if IsControlJustReleased(0, 57) and not ESX.UI.Menu.IsOpen('default', GetCurrentResourceName(), 'action_menu') then
-		OpenActionMenuInteraction()
-    end
-  end
-end)
-]]--
 
 AddEventHandler('clothesoff:openActionMenuInteraction', function()
     if not ESX.UI.Menu.IsOpen('default', GetCurrentResourceName(), 'action_menu') then
