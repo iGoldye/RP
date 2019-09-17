ca387ff785a2b48512d1efa7bc71430612c7e91a
Dan Sadler <sad2004@mail.ru>
2019-09-17 19:46:18 +0600

    CarryPeople: request

--- server-data/resources/CarryPeople/cl_carry.lua
+++ server-data/resources/CarryPeople/cl_carry.lua
@@ -1,27 +1,41 @@
+ESX                     = nil
+
+Citizen.CreateThread(function()
+  while ESX == nil do
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    Citizen.Wait(0)
+  end
+end)
+
 local carryingBackInProgress = false
 
 RegisterCommand("carry",function(source, args)
 	print("carrying")
 	if not carryingBackInProgress then
-		carryingBackInProgress = true
-		local player = PlayerPedId()	
-		lib = 'missfinale_c2mcs_1'
-		anim1 = 'fin_c2_mcs_1_camman'
-		lib2 = 'nm'
-		anim2 = 'firemans_carry'
-		distans = 0.15
-		distans2 = 0.27
-		height = 0.63
-		spin = 0.0		
-		length = 100000
-		controlFlagMe = 49
-		controlFlagTarget = 33
-		animFlagTarget = 1
-		local closestPlayer = GetClosestPlayer(3)
-		target = GetPlayerServerId(closestPlayer)
-		if closestPlayer ~= nil then
-			print("triggering cmg2_animations:sync")
-			TriggerServerEvent('cmg2_animations:sync', closestPlayer, lib,lib2, anim1, anim2, distans, distans2, height,target,length,spin,controlFlagMe,controlFlagTarget,animFlagTarget)
+		local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
+
+		if closestPlayer > 0 and closestDistance < 3.0 then
+			target = GetPlayerServerId(closestPlayer)
+
+			ESX.TriggerServerCallback('esx_request:request', function(res)
+				if res == true then
+					carryingBackInProgress = true
+					lib = 'missfinale_c2mcs_1'
+					anim1 = 'fin_c2_mcs_1_camman'
+					lib2 = 'nm'
+					anim2 = 'firemans_carry'
+					distance = 0.15
+					distance2 = 0.27
+					height = 0.63
+					spin = 0.0
+					length = 100000
+					controlFlagMe = 49
+					controlFlagTarget = 33
+					animFlagTarget = 1
+					print("triggering cmg2_animations:sync")
+					TriggerServerEvent('cmg2_animations:sync', closestPlayer, lib,lib2, anim1, anim2, distance, distance2, height,target,length,spin,controlFlagMe,controlFlagTarget,animFlagTarget)
+				end
+			end, target, "разрешение поднять вас")
 		else
 			print("[CMG Anim] No player nearby")
 		end
@@ -29,14 +43,16 @@ RegisterCommand("carry",function(source, args)
 		carryingBackInProgress = false
 		ClearPedSecondaryTask(GetPlayerPed(-1))
 		DetachEntity(GetPlayerPed(-1), true, false)
-		local closestPlayer = GetClosestPlayer(3)
-		target = GetPlayerServerId(closestPlayer)
-		TriggerServerEvent("cmg2_animations:stop",target)
+		local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
+		if closestPlayer > 0 and closestDistance < 3.0 then
+			target = GetPlayerServerId(closestPlayer)
+			TriggerServerEvent("cmg2_animations:stop",target)
+		end
 	end
 end,false)
 
 RegisterNetEvent('cmg2_animations:syncTarget')
-AddEventHandler('cmg2_animations:syncTarget', function(target, animationLib, animation2, distans, distans2, height, length,spin,controlFlag)
+AddEventHandler('cmg2_animations:syncTarget', function(target, animationLib, animation2, distance, distance2, height, length,spin,controlFlag)
 	local playerPed = GetPlayerPed(-1)
 	local targetPed = GetPlayerPed(GetPlayerFromServerId(target))
 	carryingBackInProgress = true
@@ -47,7 +63,7 @@ AddEventHandler('cmg2_animations:syncTarget', function(target, animationLib, ani
 		Citizen.Wait(10)
 	end
 	if spin == nil then spin = 180.0 end
-	AttachEntityToEntity(GetPlayerPed(-1), targetPed, 0, distans2, distans, height, 0.5, 0.5, spin, false, false, false, false, 2, false)
+	AttachEntityToEntity(GetPlayerPed(-1), targetPed, 0, distance2, distance, height, 0.5, 0.5, spin, false, false, false, false, 2, false)
 	if controlFlag == nil then controlFlag = 0 end
 	TaskPlayAnim(playerPed, animationLib, animation2, 8.0, -8.0, length, controlFlag, 0, false, false, false)
 end)
@@ -75,40 +91,3 @@ AddEventHandler('cmg2_animations:cl_stop', function()
 	DetachEntity(GetPlayerPed(-1), true, false)
 end)
 
-function GetPlayers()
-    local players = {}
-
-    for i = 0, 255 do
-        if NetworkIsPlayerActive(i) then
-            table.insert(players, i)
-        end
-    end
-
-    return players
-end
-
-function GetClosestPlayer(radius)
-    local players = GetPlayers()
-    local closestDistance = -1
-    local closestPlayer = -1
-    local ply = GetPlayerPed(-1)
-    local plyCoords = GetEntityCoords(ply, 0)
-
-    for index,value in ipairs(players) do
-        local target = GetPlayerPed(value)
-        if(target ~= ply) then
-            local targetCoords = GetEntityCoords(GetPlayerPed(value), 0)
-            local distance = GetDistanceBetweenCoords(targetCoords['x'], targetCoords['y'], targetCoords['z'], plyCoords['x'], plyCoords['y'], plyCoords['z'], true)
-            if(closestDistance == -1 or closestDistance > distance) then
-                closestPlayer = value
-                closestDistance = distance
-            end
-        end
-    end
-	print("closest player is dist: " .. tostring(closestDistance))
-	if closestDistance <= radius then
-		return closestPlayer
-	else
-		return nil
-	end
-end
--- server-data/resources/[esx]/esx_request/server/main.lua
+++ server-data/resources/[esx]/esx_request/server/main.lua
@@ -1,4 +1,41 @@
 ESX = nil
-
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+requests = {}
+
+RegisterServerEvent('esx_request:onReply')
+AddEventHandler('esx_request:onReply', function(player, res)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	if xPlayer ~= nil then
+		if res == true then
+			requests[xPlayer.source] = player
+		else
+			requests[xPlayer.source] = -1
+		end
+	end
+end)
+
+ESX.RegisterServerCallback('esx_request:request', function(source, cb, target, text)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local xTarget = ESX.GetPlayerFromId(target)
+
+	local name = GetPlayerName(source)
+
+	requests[xTarget.source] = 0
+	TriggerClientEvent('esx_request:request', target, source, name, text, 'esx_request:onReply')
+	local tm = 0
+
+	while requests[xTarget.source] == 0 and tm < 100 do
+		Citizen.Wait(100)
+		tm = tm + 1
+	end
+
+	if requests[xTarget.source] == source then
+		cb(true)
+	elseif requests[xTarget.source] == 0 then
+		cb(nil) -- no answer
+	else
+		cb(false)
+	end
+	requests[xTarget.source] = nil
+end)
