e31105c51f80c66493e5d7e9f271fa3ac866bec9
Dan Sadler <sad2004@mail.ru>
2019-09-25 15:35:50 +0600

    esx_whitelist: maintenance mode

--- server-data/resources/[esx]/esx_whitelist/config.lua
+++ server-data/resources/[esx]/esx_whitelist/config.lua
@@ -6,3 +6,7 @@ Config.EnableAntiSpam = true
 Config.WaitingTime = 5
 Config.CommunityLink = 'COMMUNITY_LINK'
 Config.PlayersToStartRocade = 32
+
+-- не пускаем игроков с 9:00 до 18:00
+Config.MaintenanceStart = 9
+Config.MaintenanceEnd = 18
