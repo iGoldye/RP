fc3329cdbffdbc6299e72d6feb04ece5d42bffef
skeev_ <derkach94@gmail.com>
2019-10-02 17:26:26 -0700

    basicneeds: beer

--- server-data/resources/[esx]/esx_basicneeds/server/itemeffects.lua
+++ server-data/resources/[esx]/esx_basicneeds/server/itemeffects.lua
@@ -242,6 +242,17 @@ ESX.RegisterUsableItem('drug3', function(source)
 
 end)
 
+ESX.RegisterUsableItem('beer', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('beer', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'drunk', 300000)
+    TriggerClientEvent('esx_optionalneeds:onDrink', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_beer'))
+
+end)
+
 ESX.RegisterUsableItem('cigarette', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
 
