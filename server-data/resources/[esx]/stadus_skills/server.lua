e8095fe06a812de9ae57fb5c2a8f94934ebe2f58
Dan Sadler <sad2004@mail.ru>
2019-10-05 19:31:16 +0600

    stadus_skills, esx_gym: skill system

--- server-data/resources/[esx]/stadus_skills/server.lua
+++ server-data/resources/[esx]/stadus_skills/server.lua
@@ -12,7 +12,7 @@ AddEventHandler('esx:playerLoaded', function(source)
   local xPlayer  = ESX.GetPlayerFromId(_source)
 	MySQL.Async.fetchAll('SELECT * FROM `stadus_skills` WHERE `identifier` = @identifier', {['@identifier'] = xPlayer.identifier}, function(skillInfo)
 		if ( skillInfo and skillInfo[1] ) then
-			TriggerClientEvent('stadus_skills:sendPlayerSkills', _source, skillInfo[1].stamina, skillInfo[1].strength, skillInfo[1].driving, skillInfo[1].shooting, skillInfo[1].fishing, skillInfo[1].drugs)
+			TriggerClientEvent('stadus_skills:sendPlayerSkills', _source, skillInfo[1])
 			else
 				MySQL.Async.execute('INSERT INTO `stadus_skills` (`identifier`, `strength`, `stamina`, `driving`, `shooting`, `fishing`, `drugs`) VALUES (@identifier, @strength, @stamina, @driving, @shooting, @fishing, @drugs);',
 				{
@@ -34,7 +34,7 @@ function updatePlayerInfo(source)
   local xPlayer  = ESX.GetPlayerFromId(_source)
 	MySQL.Async.fetchAll('SELECT * FROM `stadus_skills` WHERE `identifier` = @identifier', {['@identifier'] = xPlayer.identifier}, function(skillInfo)
 		if ( skillInfo and skillInfo[1] ) then
-			TriggerClientEvent('stadus_skills:sendPlayerSkills', _source, skillInfo[1].stamina, skillInfo[1].strength, skillInfo[1].driving, skillInfo[1].shooting, skillInfo[1].fishing, skillInfo[1].drugs)
+			TriggerClientEvent('stadus_skills:sendPlayerSkills', _source, skillInfo[1])
 		end
 	end)
 end
@@ -42,7 +42,7 @@ end
 RegisterServerEvent("stadus_skills:addStamina")
 AddEventHandler("stadus_skills:addStamina", function(source, amount)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	TriggerClientEvent('esx:showNotification', source, 'Вы чувствуете себя на ~g~' .. round(amount, 2) .. '% ~s~быстрее!')
+--	TriggerClientEvent('esx:showNotification', source, 'Вы чувствуете себя на ~g~' .. round(amount, 2) .. '% ~s~быстрее!')
 	MySQL.Async.fetchAll('SELECT * FROM `stadus_skills` WHERE `identifier` = @identifier', {['@identifier'] = xPlayer.identifier}, function(skillInfo)
 		MySQL.Async.execute('UPDATE `stadus_skills` SET `stamina` = @stamina WHERE `identifier` = @identifier',
 			{
@@ -57,7 +57,7 @@ end)
 RegisterServerEvent("stadus_skills:addStrength")
 AddEventHandler("stadus_skills:addStrength", function(source, amount)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	TriggerClientEvent('esx:showNotification', source, 'Вы чувствуете себя на ~g~' .. round(amount, 2) .. '% ~s~сильнее!')
+--	TriggerClientEvent('esx:showNotification', source, 'Вы чувствуете себя на ~g~' .. round(amount, 2) .. '% ~s~сильнее!')
 	MySQL.Async.fetchAll('SELECT * FROM `stadus_skills` WHERE `identifier` = @identifier', {['@identifier'] = xPlayer.identifier}, function(skillInfo)
 		MySQL.Async.execute('UPDATE `stadus_skills` SET `strength` = @strength WHERE `identifier` = @identifier',
 			{
@@ -87,7 +87,7 @@ end)
 RegisterServerEvent("stadus_skills:addFishing")
 AddEventHandler("stadus_skills:addFishing", function(source, amount)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	TriggerClientEvent('esx:showNotification', source, 'Вы совершенствуете на ~g~' .. round(amount, 2) .. '% ~s~мастерство рыбалки!')
+--	TriggerClientEvent('esx:showNotification', source, 'Вы совершенствуете на ~g~' .. round(amount, 2) .. '% ~s~мастерство рыбалки!')
 	MySQL.Async.fetchAll('SELECT * FROM `stadus_skills` WHERE `identifier` = @identifier', {['@identifier'] = xPlayer.identifier}, function(skillInfo)
 		MySQL.Async.execute('UPDATE `stadus_skills` SET `fishing` = @fishing WHERE `identifier` = @identifier',
 			{
@@ -102,7 +102,7 @@ end)
 RegisterServerEvent("stadus_skills:addDrugs")
 AddEventHandler("stadus_skills:addDrugs", function(source, amount)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	TriggerClientEvent('esx:showNotification', source, 'Вы совершенстувете навык ~g~' .. round(amount, 2) .. '% ~s~производства ~y~наркотиков~s~!')
+--	TriggerClientEvent('esx:showNotification', source, 'Вы совершенстувете навык ~g~' .. round(amount, 2) .. '% ~s~производства ~y~наркотиков~s~!')
 	MySQL.Async.fetchAll('SELECT * FROM `stadus_skills` WHERE `identifier` = @identifier', {['@identifier'] = xPlayer.identifier}, function(skillInfo)
 		MySQL.Async.execute('UPDATE `stadus_skills` SET `drugs` = @drugs WHERE `identifier` = @identifier',
 			{
@@ -117,6 +117,6 @@ end)
 ESX.RegisterServerCallback('stadus_skills:getSkills', function(source, cb)
   local xPlayer    = ESX.GetPlayerFromId(source)
 	MySQL.Async.fetchAll('SELECT * FROM `stadus_skills` WHERE `identifier` = @identifier', {['@identifier'] = xPlayer.identifier}, function(skillInfo)
-		cb(skillInfo[1].stamina, skillInfo[1].strength, skillInfo[1].driving, skillInfo[1].shooting, skillInfo[1].fishing, skillInfo[1].drugs)
+		cb(skillInfo[1])
 	end)
 end)
