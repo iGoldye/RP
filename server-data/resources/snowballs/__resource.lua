89a252ae121a57c124ef751100b36f28c0bb1ee9
skeev_ <derkach94@gmail.com>
2019-11-20 20:34:00 +0300

    New resource: snowballs

---
+++ server-data/resources/snowballs/__resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_script 'client.lua'
+
+files {
+  'weapon_snowball.meta',
+}
+
+data_file 'WEAPONINFO_FILE' 'weapon_snowball.meta'
