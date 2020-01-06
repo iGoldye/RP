7623e5b299c50ca760fb6bc3407fb8ee924f5868
Dan Sadler <sad2004@mail.ru>
2020-01-06 17:41:20 +0600

    es_extended: backport new DrawText3D

--- server-data/resources/[esx]/loffe_robbery1/client.lua
+++ server-data/resources/[esx]/loffe_robbery1/client.lua
@@ -331,17 +331,5 @@ loadDict = function(dict)
 end
 
 function DrawText3D(coords, text)
-    local onScreen, _x, _y = World3dToScreen2d(coords.x, coords.y, coords.z)
-    local pX, pY, pZ = table.unpack(GetGameplayCamCoords())
-
-    SetTextScale(0.4, 0.4)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextEntry("STRING")
-    SetTextCentre(1)
-    SetTextColour(255, 255, 255, 255)
-    SetTextOutline()
-
-    AddTextComponentString(text)
-    DrawText(_x, _y)
+    ESX.Game.Utils.DrawText3D(coords, text, 0.5, 4)
 end
